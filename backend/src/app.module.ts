b0c4cf1a089c86c0e069126a0563b0d36b6f31a5
Aman Patel <patel.26@iitj.ac.in>
2025-02-19 22:41:12 +0530

    docker setup done

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,23 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>('MONGODB_URI') || '',
+        };
+      },
+    }),
     AuthModule,
     UserModule,
     TasksModule,
-    MongooseModule.forRoot(
-      'mongodb://admin:173314@localhost:27017/streamhatchet?directConnection=true&authSource=admin&replicaSet=replicaset&retryWrites=true',
-    ),
   ],
 })
 export class AppModule {}
